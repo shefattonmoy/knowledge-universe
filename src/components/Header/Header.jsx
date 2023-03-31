4d4a01be355033737e9ef098827d07475b6c1505
Shefat Tonmoy <shefathossain7@gmail.com>
2023-03-31 15:15:25 +0600

    header section done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css';
+import image from '../../images/shefat_circle_image.png';
+
+const Header = () => {
+    return (
+        <nav className='header'>
+            <div>
+                <h2>Knowledge Cafe</h2>
+            </div>
+            <div>
+                <a href="/home">Home</a>
+                <a href="/blog">Blog</a>
+                <a href="/login">Login</a>
+                <img className='nav-image' src={image} alt="" />
+            </div>
+        </nav>
+    );
+};
+
+export default Header;

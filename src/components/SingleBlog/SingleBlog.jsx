9a1c63af7902d5bff58f8f2c378cc3316a4d679b
Shefat Tonmoy <shefathossain7@gmail.com>
2023-03-31 22:52:37 +0600

    some minor fixes

--- src/components/SingleBlog/SingleBlog.jsx
+++ src/components/SingleBlog/SingleBlog.jsx
@@ -5,13 +5,29 @@ import './SingleBlog.css';
 
 
 const SingleBlog = (props) => {
-    const {img, name, title, estimatedTime, image} = props.singleBlog
+    const { img, name, title, estimatedTime, authorImg } = props.singleBlog
     return (
         <div className='singleBlog'>
             <img src={img} alt="" />
-            <button>{estimatedTime}<FontAwesomeIcon icon={faBookmark} /></button>
-            <h4 className='singleBlog-title'>{title}</h4>
-            <a href="">Mark as read</a>
+            <div className='d-flex justify-content-between fs-6 fw-semibold'>
+                <div className='singleBlog-info-child-1'>
+                    <img src={authorImg} alt="" />
+                    <p>{name}</p>
+                    <p className='text-secondary'>March 14, 2023</p>
+                </div>
+                <div className='d-flex gap-2'>
+                    <div>
+                        <p className='text-secondary'>{estimatedTime}</p>
+                    </div>
+                    <div>
+                        <button className='btn btn-light'><FontAwesomeIcon icon={faBookmark} /></button>
+                    </div>
+                </div>
+            </div>
+            <div className='singleBlog-info-child-2'>
+                <h4 className='singleBlog-title'>{title}</h4>
+                <a href="">Mark as read</a>
+            </div>
         </div>
     );
 };
